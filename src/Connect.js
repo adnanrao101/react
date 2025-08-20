79dea47cd6203ca9cf7901f8b3595bff6eb24257
Adnan Rao <adnanejaz2874@gmail.com>
2025-08-20 14:45:58 +0500

    react

---
+++ src/Connect.js
@@ -0,0 +1,11 @@
+import './App.css';
+import React from 'react';
+function Connect() {
+  return (
+    <>
+   <h1>this is 3rd component</h1>
+   <h2>iikujhkujijk</h2>
+   </>
+  );
+}
+export default Connect;
---
+++ src/Contact.js
@@ -0,0 +1,88 @@
+function Contact(props) {
+  let multi = props.num1 * props.num2
+
+  return(
+    <>
+    <h1>{props.num1} * {props.num2} = {multi}</h1>
+    </>
+  )
+}
+
+export default Contact;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Contact(props){
+// let sum = props.num1 + props.num2
+// return(
+//   <>
+//   <h1>{props.num1}+{props.num2} = {sum}</h1>
+//   </>
+// )
+// }
+
+// export default Contact;
